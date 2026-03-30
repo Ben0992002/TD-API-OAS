185addd3552bfeea7c1016919eb16802df726142
Ben0992002 <hei.issa.29@gmail.com>
2026-03-30 09:24:40 +0300

    Correction et version finale du TD4

--- src/main/java/hei/school/demo/controller/HelloController.java
+++ src/main/java/hei/school/demo/controller/HelloController.java
@@ -1,13 +1,25 @@
 package hei.school.demo.controller;
 
+import hei.school.demo.exception.BadRequestException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Hello Spring Boot 🚀";
+    // L'URL sera http://localhost:8080/hello
+    @GetMapping("/hello")
+    public String home(@RequestParam(required = false) String name) {
+
+        // 1. controller vérifie si le message est vide
+        if (name != null && name.trim().isEmpty()) {
+
+            // 2. Lancement de l'alerte !!!
+            throw new BadRequestException("Le nom ne peut pas être vide");
+        }
+
+        // 3. Si tout va bien, on répond normalement
+        return "Hello " + (name == null ? "Spring Boot 🚀" : name);
     }
 }
